b33e1e31b67ddcb55aeaef4dcccd997f1c432754
Daniel Stucki <dstucki@hsr.ch>
2012-05-28 16:23:30 +0200

    added javadoc

--- src/ch/hsr/sa/radiotour/fragments/DriverGroupFragment.java
+++ src/ch/hsr/sa/radiotour/fragments/DriverGroupFragment.java
@@ -18,16 +18,18 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import ch.hsr.sa.radiotour.R;
 import ch.hsr.sa.radiotour.activities.RadioTourActivity;
-import ch.hsr.sa.radiotour.application.RadioTour;
 import ch.hsr.sa.radiotour.domain.Group;
-import ch.hsr.sa.radiotour.domain.RiderStageConnection;
 import ch.hsr.sa.radiotour.domain.RiderState;
 import ch.hsr.sa.radiotour.fragments.controller.DriverGroupController;
-import ch.hsr.sa.radiotour.technicalservices.connection.JsonSendingQueue;
 import ch.hsr.sa.radiotour.technicalservices.listener.DriverGroupClickListener;
 import ch.hsr.sa.radiotour.technicalservices.listener.GroupingDragListener;
 import ch.hsr.sa.radiotour.views.GroupTableRow;
 
+/**
+ * 
+ * Class to display the area where the Riders are grouped
+ * 
+ */
 public class DriverGroupFragment extends Fragment {
 	private GroupingDragListener dragListener;
 	private DriverGroupClickListener clickListener;
@@ -42,68 +44,10 @@ public class DriverGroupFragment extends Fragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		controller = new DriverGroupController(this);
-
 		clearOldInformation();
-		assignListener();
+		assignDragListener();
 		setOnClickListener();
 		initializeTableRows();
-	}
-
-	private void clearOldInformation() {
-		tableRows.clear();
-		driverTableRow.clear();
-	}
-
-	private void assignListener() {
-		getView().findViewById(R.id.tableRowGroup).setOnDragListener(
-				dragListener);
-		getView().findViewById(R.id.tableRowField).setOnDragListener(
-				dragListener);
-		getView().findViewById(R.id.tableRowGroup2).setOnDragListener(
-				dragListener);
-	}
-
-	private void initializeDriverRowMap() {
-		for (Integer i : controller.getRiderNumbers()) {
-			driverTableRow.put(i, field);
-		}
-
-	}
-
-	private void initializeTableRows() {
-		tableRows.add((TableRow) getView().findViewById(R.id.tableRowGroup));
-		field = (GroupTableRow) getView().findViewById(R.id.tableRowField);
-		tableRows.add(field);
-		initializeDriverRowMap();
-		field.changeDescription(getString(R.string.field));
-		tableRows.add((TableRow) getView().findViewById(R.id.tableRowGroup2));
-		standardParams = (TableLayout.LayoutParams) tableRows.get(0)
-				.getLayoutParams();
-		standardRowParams = (TableRow.LayoutParams) tableRows.get(0)
-				.getChildAt(0).getLayoutParams();
-		if (controller.getGroups().isEmpty()) {
-			field.setGroup(controller.createFieldGroup());
-		} else {
-			alreadyGroupsHere();
-		}
-	}
-
-	private void alreadyGroupsHere() {
-		int counter = 0;
-		for (Group g : controller.getGroups()) {
-			if (!g.isField()) {
-				GroupTableRow tempRow = createNewGroup(counter);
-				tempRow.setGroup(g);
-				for (int i : g.getDriverNumbers()) {
-					field.removeRiderNr(i);
-					tempRow.addRider(i);
-					driverTableRow.put(i, tempRow);
-				}
-			} else {
-				field.setGroup(g);
-			}
-			counter += 2;
-		}
 	}
 
 	@Override
@@ -113,6 +57,32 @@ public class DriverGroupFragment extends Fragment {
 		return view;
 	}
 
+	/**
+	 * clear all the class intern information to avoid duplication
+	 */
+	private void clearOldInformation() {
+		tableRows.clear();
+		driverTableRow.clear();
+	}
+
+	/**
+	 * Assign the {@link GroupingDragListener} object to the three predefined
+	 * {@link TableRow} objects in the layout
+	 */
+
+	private void assignDragListener() {
+		getView().findViewById(R.id.tableRowGroup).setOnDragListener(
+				dragListener);
+		getView().findViewById(R.id.tableRowField).setOnDragListener(
+				dragListener);
+		getView().findViewById(R.id.tableRowGroup2).setOnDragListener(
+				dragListener);
+	}
+
+	/**
+	 * Assign the {@link DriverGroupClickListener} object to the three
+	 * predefined {@link TableRow} objects in the layout
+	 */
 	private void setOnClickListener() {
 		clickListener = new DriverGroupClickListener();
 		getView().findViewById(R.id.tableRowGroup).setOnClickListener(
@@ -124,10 +94,75 @@ public class DriverGroupFragment extends Fragment {
 		clickListener.addObserver((RadioTourActivity) getActivity());
 	}
 
+	/**
+	 * Initial fill the driverTableRow {@link SparseArray} with the value of the
+	 * field
+	 */
+	private void initializeDriverRowMap() {
+		for (Integer i : controller.getRiderNumbers()) {
+			driverTableRow.put(i, field);
+		}
+
+	}
+
+	/**
+	 * Initial add predefined Layout objects to the class intern datahandling
+	 */
+	private void initializeTableRows() {
+		tableRows.add((TableRow) getView().findViewById(R.id.tableRowGroup));
+		field = (GroupTableRow) getView().findViewById(R.id.tableRowField);
+		tableRows.add(field);
+		field.changeDescription(getString(R.string.field));
+		tableRows.add((TableRow) getView().findViewById(R.id.tableRowGroup2));
+		standardParams = (TableLayout.LayoutParams) tableRows.get(0)
+				.getLayoutParams();
+		standardRowParams = (TableRow.LayoutParams) tableRows.get(0)
+				.getChildAt(0).getLayoutParams();
+		initializeDriverRowMap();
+		if (controller.getGroups().isEmpty()) {
+			field.setGroup(controller.createFieldGroup());
+		} else {
+			alreadyGroupsHere();
+		}
+	}
+
+	/**
+	 * Add for existing Groups a {@link GroupTableRow}
+	 */
+	private void alreadyGroupsHere() {
+		int counter = 0;
+		for (Group g : controller.getGroups()) {
+			if (!g.isField()) {
+				GroupTableRow tempRow = createNewGroup(counter);
+				tempRow.setGroup(g);
+				for (int i : g.getDriverNumbers()) {
+					removeAndAddRiderNr(tempRow, i);
+				}
+			} else {
+				field.setGroup(g);
+			}
+			counter += 2;
+		}
+	}
+
+	/**
+	 * set the {@link GroupingDragListener}
+	 * 
+	 * @param listener
+	 */
 	public void setDragListener(GroupingDragListener listener) {
 		dragListener = listener;
 	}
 
+	/**
+	 * Creates two {@link TableRow}, one that can accept Rider and one that acts
+	 * as a layout to create a new group. Additionally it adds those two layouts
+	 * tho the parent layouts
+	 * 
+	 * @param indexOf
+	 *            where to create a new group
+	 * @return the layout that accepts Rider
+	 */
 	private GroupTableRow createNewGroup(int indexOf) {
 		TableLayout layout = (TableLayout) ((ScrollView) getView())
 				.findViewById(R.id.groupTableLayout);
@@ -141,6 +176,11 @@ public class DriverGroupFragment extends Fragment {
 		return groupRow;
 	}
 
+	/**
+	 * Creates the {@link GroupTableRow} that accepts riders and returns it
+	 * 
+	 * @return {@link GroupTableRow}
+	 */
 	private GroupTableRow createGroupRow() {
 		final GroupTableRow row = new GroupTableRow(getActivity());
 		row.setLayoutParams(standardParams);
@@ -150,6 +190,11 @@ public class DriverGroupFragment extends Fragment {
 		return row;
 	}
 
+	/**
+	 * Creates the {@link GroupTableRow} that is used to create new group
+	 * 
+	 * @return {@link TableRow}
+	 */
 	private TableRow createNewGroupRow() {
 		TableRow row = new TableRow(getActivity());
 		TextView test = new TextView(getActivity());
@@ -164,6 +209,10 @@ public class DriverGroupFragment extends Fragment {
 		return row;
 	}
 
+	/**
+	 * remove TableRows that aren't used anymore. additionally it cleans up the
+	 * descriptions and syncs the situation to the database
+	 */
 	private void removeEmptyTableRows() {
 		GroupTableRow temp;
 		for (int i = 1; i < tableRows.size(); i += 2) {
@@ -183,12 +232,16 @@ public class DriverGroupFragment extends Fragment {
 		syncToDb();
 	}
 
+	/**
+	 * sets the right descriptions to the {@link GroupTableRow} objects
+	 */
 	private void assignNameToTableRow() {
 		int locationOfField = tableRows.indexOf(field);
 		int counter = 1;
 		String prefix = getString(R.string.top);
+		GroupTableRow temp;
 		if (locationOfField > counter) {
-			GroupTableRow temp = ((GroupTableRow) tableRows.get(counter));
+			temp = ((GroupTableRow) tableRows.get(counter));
 			temp.getGroup().setLeader(true);
 			temp.setTime(new Date(0, 0, 0, 0, 0, 0));
 			temp.changeDescription(prefix);
@@ -204,20 +257,23 @@ public class DriverGroupFragment extends Fragment {
 		counter += 2;
 		prefix = getString(R.string.follower);
 		for (; counter < locationOfField; counter += 2) {
-			GroupTableRow temp = ((GroupTableRow) tableRows.get(counter));
+			temp = ((GroupTableRow) tableRows.get(counter));
 			temp.changeDescription(prefix + ((counter / 2) + 1));
 			temp.getGroup().setLeader(false);
 
 		}
 		prefix = getString(R.string.detached);
 		for (counter = locationOfField + 2; counter < tableRows.size(); counter += 2) {
-			GroupTableRow temp = ((GroupTableRow) tableRows.get(counter));
+			temp = ((GroupTableRow) tableRows.get(counter));
 			temp.changeDescription(prefix + ((counter - locationOfField) / 2));
 			temp.getGroup().setLeader(false);
 		}
 
 	}
 
+	/**
+	 * syncs the situation to the db
+	 */
 	private void syncToDb() {
 		List<Group> groups = new LinkedList<Group>();
 		for (int i = 1; i < tableRows.size(); i += 2) {
@@ -228,10 +284,25 @@ public class DriverGroupFragment extends Fragment {
 		controller.saveGroups(groups);
 	}
 
+	/**
+	 * determines if the provided {@link TableRow} is one that accepts rider or
+	 * one that is used to create a new group
+	 * 
+	 * @param row
+	 * @return <code>true</code> if it has to create a new group, otherwise
+	 *         <code>false</code>
+	 */
 	private boolean hasToCreateNewGroup(TableRow row) {
 		return tableRows.indexOf(row) % 2 == 0;
 	}
 
+	/**
+	 * handles the placement of the given ridernumbers
+	 * 
+	 * @param destination
+	 *            {@link View} that the ridernumbers will go to
+	 * @param riderNumbers
+	 */
 	public void moveDriverNr(View destination, Set<Integer> riderNumbers) {
 		if (riderNumbers == null || riderNumbers.isEmpty()) {
 			return;
@@ -251,9 +322,7 @@ public class DriverGroupFragment extends Fragment {
 			if (groupTableRow == null) {
 				selectOnATextView((TextView) destination, i);
 			} else {
-				removeDriver(i);
-				groupTableRow.addRider(i);
-				driverTableRow.put(i, groupTableRow);
+				removeAndAddRiderNr(groupTableRow, i);
 
 			}
 		}
@@ -261,7 +330,33 @@ public class DriverGroupFragment extends Fragment {
 
 	}
 
-	private void selectOnATextView(TextView v, int i) {
+	/**
+	 * removes the given ridernr from the old location and adds it on the new
+	 * {@link GroupTableRow}
+	 * 
+	 * @param groupTableRow
+	 *            the new {@link GroupTableRow}
+	 * @param ridernr
+	 *            the ridernr that has to be displaced
+	 */
+	private void removeAndAddRiderNr(GroupTableRow groupTableRow,
+			Integer ridernr) {
+		driverTableRow.get(ridernr).removeRiderNr(ridernr);
+		groupTableRow.addRider(ridernr);
+		driverTableRow.put(ridernr, groupTableRow);
+	}
+
+	/**
+	 * handles a click or drag on a special event {@link TextView}
+	 * 
+	 * @param v
+	 *            {@link TextView} that has been clicked or on that there was a
+	 *            drop
+	 * @param riderNr
+	 *            the ridernr as for the special event occurs
+	 * 
+	 */
+	private void selectOnATextView(TextView v, int riderNr) {
 		RiderState newState = RiderState.ACTIV;
 		switch (v.getId()) {
 		case R.id.arzt:
@@ -275,24 +370,14 @@ public class DriverGroupFragment extends Fragment {
 			break;
 		case R.id.aufgabe:
 			newState = RiderState.GIVEUP;
-			removeDriver(i);
+			driverTableRow.get(riderNr).removeRiderNr(riderNr);
 			break;
 		case R.id.not_started:
 			newState = RiderState.NOT_STARTED;
-			removeDriver(i);
+			driverTableRow.get(riderNr).removeRiderNr(riderNr);
 			break;
 		}
-		RiderStageConnection riderStageConnection = ((RadioTour) getActivity()
-				.getApplication()).getRiderStage(i);
-		riderStageConnection.setRiderState(newState);
-		controller.update(riderStageConnection);
-		JsonSendingQueue.getInstance().addToQueue(riderStageConnection);
-	}
-
-	private void removeDriver(int i) {
-		if (driverTableRow.get(i) != null) {
-			driverTableRow.get(i).removeRiderNr(i);
-		}
+		controller.setRiderState(riderNr, newState);
 	}
 
 }
--- src/ch/hsr/sa/radiotour/fragments/controller/DriverGroupController.java
+++ src/ch/hsr/sa/radiotour/fragments/controller/DriverGroupController.java
@@ -7,6 +7,8 @@ import ch.hsr.sa.radiotour.application.RadioTour;
 import ch.hsr.sa.radiotour.domain.Group;
 import ch.hsr.sa.radiotour.domain.RaceSituation;
 import ch.hsr.sa.radiotour.domain.RiderStageConnection;
+import ch.hsr.sa.radiotour.domain.RiderState;
+import ch.hsr.sa.radiotour.domain.Stage;
 import ch.hsr.sa.radiotour.fragments.DriverGroupFragment;
 import ch.hsr.sa.radiotour.fragments.HeaderFragment;
 import ch.hsr.sa.radiotour.technicalservices.connection.JsonSendingQueue;
@@ -14,89 +16,196 @@ import ch.hsr.sa.radiotour.technicalservices.database.DatabaseHelper;
 
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 
+/**
+ * Class that acts as a Controller for {@link DriverGroupFragment}
+ */
 public class DriverGroupController {
 	private final RadioTour app;
 	private RuntimeExceptionDao<Group, Integer> groupDao;
 	private RuntimeExceptionDao<RaceSituation, Long> situationDao;
 	private RuntimeExceptionDao<RiderStageConnection, Integer> riderStageDao;
 
+	/**
+	 * Public Constructor
+	 * 
+	 * @param fragment
+	 *            to which this controller is assigned
+	 */
 	public DriverGroupController(DriverGroupFragment fragment) {
 		this.app = (RadioTour) fragment.getActivity().getApplication();
 		assignDaos(DatabaseHelper.getHelper(fragment.getActivity()));
 	}
 
+	/**
+	 * assigns the dao's fields
+	 * 
+	 * @param helper
+	 *            {@link DatabaseHelper} instance
+	 */
 	private void assignDaos(DatabaseHelper helper) {
 		groupDao = helper.getGroupDao();
 		situationDao = helper.getRaceSituationDao();
 		riderStageDao = helper.getRiderStageDao();
-
 	}
 
+	/**
+	 * Persists a {@link List} of {@link Group} objects to the Database
+	 * 
+	 * @param gr
+	 *            {@link Group} objects to be persisted
+	 */
 	private void create(List<Group> gr) {
 		for (Group g : gr) {
 			groupDao.create(g);
 		}
 	}
 
+	/**
+	 * Persists given {@link RaceSituation} object and the assigned
+	 * {@link Group} objects to the Database. Adds the given
+	 * {@link RaceSituation} object to the {@link JsonSendingQueue}
+	 * 
+	 * @param sit
+	 *            {@link RaceSituation} that will be persisted and sent to the
+	 *            server
+	 */
 	private void create(RaceSituation sit) {
 		situationDao.create(sit);
 		create(sit.getGroups());
 		JsonSendingQueue.getInstance().addToQueue(sit);
 	}
 
+	/**
+	 * Persist the given Object to Database
+	 * 
+	 * @param conn
+	 *            {@link RiderStageConnection} to be persisted
+	 */
 	public void update(RiderStageConnection conn) {
 		riderStageDao.update(conn);
 	}
 
-	public List<Integer> getRiderNumbers() {
+	/**
+	 * Get all Starting Numbers that are stored in {@link RadioTour}
+	 * 
+	 * @return a {@link Collection} of {@link Integer}
+	 */
+	public Collection<Integer> getRiderNumbers() {
 		return app.getRiderNumbers();
 	}
 
-	public List<Group> getGroups() {
+	/**
+	 * Get all Groups that are stored in {@link RadioTour}
+	 * 
+	 * @return a {@link Collection} of {@link Group}
+	 */
+	public Collection<Group> getGroups() {
 		return app.getGroups();
 	}
 
-	private void add(Group g) {
-		app.getSituation().add(g);
+	/**
+	 * gets the {@link RiderStageConnection} and changes the {@link RiderState}
+	 * and persists it to the Database
+	 * 
+	 * @param riderNr
+	 *            number of the rider
+	 * @param state
+	 *            new state that the {@link RiderStageConnection} will have
+	 */
+	public void setRiderState(int riderNr, RiderState state) {
+		RiderStageConnection riderStage = app.getRiderStage(riderNr);
+		riderStage.setRiderState(state);
+		JsonSendingQueue.getInstance().addToQueue(riderStage);
+		update(riderStage);
 	}
 
+	/**
+	 * Creates and returns the Field {@link Group} containing all of the
+	 * RiderNummbers
+	 * 
+	 * @return
+	 */
 	public Group createFieldGroup() {
 		Group gr = new Group();
 		gr.setSituation(app.getSituation());
 		gr.setField(true);
-		gr.getDriverNumbers().addAll(app.getRiderNumbers());
+		gr.getDriverNumbers().addAll(getRiderNumbers());
 		gr.setOrderNumber(0);
-		add(gr);
 		return gr;
 	}
 
+	/**
+	 * Persists all the {@link RiderStageConnection} that are assigned to the
+	 * actual {@link Stage} to Database
+	 */
 	private void updateConns() {
-		Collection<Integer> riderNrs = app.getRiderNumbers();
+		Collection<Integer> riderNrs = getRiderNumbers();
 		for (int i : riderNrs) {
 			update(app.getRiderStage(i));
 		}
 
 	}
 
+	/**
+	 * Creates and return a new RaceSituation with the appropiate value for
+	 * {@link RaceSituation} distance and Stage
+	 * 
+	 * @return the new createt {@link RaceSituation} object
+	 */
 	private RaceSituation getNewSituation() {
 		return new RaceSituation(HeaderFragment.mGPS.getDistanceInKm(),
 				app.getActualSelectedStage());
 	}
 
+	/**
+	 * Creates a {@link RaceSituation} object, adds the groups and persists it.
+	 * Additionally it adds the Situation to the {@link JsonSendingQueue}
+	 * 
+	 * @param groups
+	 *            all the groups that should be in the current
+	 *            {@link RaceSituation}
+	 */
 	public void saveGroups(List<Group> groups) {
 		runAsThread(new SaveGroups(groups), "saveGroups");
 	}
 
-	private void runAsThread(SaveGroups saveGroups, String name) {
-		new Thread(saveGroups, name).start();
+	/**
+	 * Helper Class to wrap a {@link Runnable} and run it as a {@link Thread}
+	 * 
+	 * @param runnable
+	 *            That will be ran from the {@link Thread}
+	 * @param name
+	 *            Name that will be assigned to the {@link Thread}
+	 */
+	private void runAsThread(Runnable runnable, String name) {
+		new Thread(runnable, name).start();
 	}
 
+	/**
+	 * Class implementing {@link Runnable} that makes all the saving of a
+	 * {@link List} of Groups inclusive creating {@link RaceSituation} and
+	 * persist the {@link RiderStageConnection}
+	 * 
+	 */
 	private class SaveGroups implements Runnable {
 		private final List<Group> groups;
 
+		/**
+		 * public constructor
+		 * 
+		 * @param groups
+		 *            {@link List} of {@link Group} that will be packed into a
+		 *            {@link RaceSituation} and be sent to the Server
+		 */
 		public SaveGroups(List<Group> groups) {
 			this.groups = groups;
 		}
+
+		/**
+		 * Does all the synchronization work for a given {@link List} of
+		 * {@link Group} and initiates the sending to the server using the
+		 * {@link JsonSendingQueue}
+		 */
 
 		@Override
 		public void run() {
@@ -108,6 +217,7 @@ public class DriverGroupController {
 				situation.add(gr);
 			}
 			create(situation);
+			app.setSituation(situation);
 			updateConns();
 
 		}
